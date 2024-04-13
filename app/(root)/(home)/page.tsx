3f417c7666fdbb73fd87e6fb31ca6c9e58c197d4
berdibek-2002 <uralov0212@gmail.com>
2024-04-13 18:11:22 +0500

    version 1.0.5

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,3 @@
-import Auth from '@/components/auth';
-
 export default function Home() {
-  const user = false;
-
-  if (!user)
-    return (
-      <div className='container h-screen mx-auto max-w-7xl'>
-        <Auth />
-      </div>
-    );
-
   return <div>Home</div>;
 }
---
+++ hooks/useUsers.ts
@@ -0,0 +1,15 @@
+import fetcher from '@/lib/fetcher';
+import useSWR from 'swr';
+
+const useUsers = (limit: number) => {
+  const { data, error, isLoading, mutate } = useSWR(`/api/users?limit=${limit}`, fetcher);
+
+  return {
+    users: data,
+    isLoading,
+    isError: error,
+    mutate,
+  };
+};
+
+export default useUsers;
